e0df3040ed58ea3283803de2fb9cea03a72cb4a4
haylengonzalez <haylengonzalez@colegioguanenta.edu.co>
2022-10-26 07:03:44 -0500

    se agrega y se hacen algunos ejercicios en la carpeta tuplas

---
+++ tuplas/ejercicios.py
@@ -0,0 +1,24 @@
+# EJERCICIOS TUPLAS
+
+# 1. 
+tupla = ("Casa", "2", 345, "Perro", 99)
+print("Elementos de la tupla: ",tupla)
+print("Elemento de la posición 0: ",tupla[0])
+print("Elemento de la posición 1: ",tupla[1])
+print("Elemento de la posición 2: ",tupla[2])
+print("Elemento de la posición 3: ",tupla[3])
+print("Elemento de la posición 4: ",tupla[4])
+
+# 2.
+tupla = ("Casa", "2", 99, 345, "Perro", 99)
+print("Elementos de la tupla: ",tupla)
+print("Número de elementos 99: ",tupla.count(99))
+print("Posición que ocupa el elemento Perro: ",tupla.index("Perro"))
+tupla = (1,2,3,4,5,6,7,8,9)
+print(tupla)
+print(tupla[4:9])
+print(tupla[:3])
+print(tupla[2:])
+
+# 3. 
+tupla = (1,2,3,4,5,6,7)
